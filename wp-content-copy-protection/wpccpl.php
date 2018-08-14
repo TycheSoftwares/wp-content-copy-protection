72eda12ac2bf7100de86bbab4dc673df422af9f2
bhavik <bhavik@bhavik>
2018-08-14 14:13:41 +0530

    Spell mistake
    spell mistake

--- wp-content-copy-protection/wpccpl.php
+++ wp-content-copy-protection/wpccpl.php
@@ -227,7 +227,7 @@ img {
 <script type="text/javascript">
 window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==70||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==70||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
 /**
- * For mac we need to ceck metakey
+ * For mac we need to check metakey
  */
 window.addEventListener("keydown",function(e){if( event.metaKey&&(e.which==65||e.which==66||e.which==67||e.which==70||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==70||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
 
