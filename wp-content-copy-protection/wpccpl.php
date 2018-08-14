0b50f2c8c5e18f525ca07b6a391475e8a3d0ef14
bhavik <bhavik@bhavik>
2018-08-14 14:12:34 +0530

    Fix #11
    This fix ensures that it will now allow to print page when your are using mac.

--- wp-content-copy-protection/wpccpl.php
+++ wp-content-copy-protection/wpccpl.php
@@ -226,7 +226,13 @@ img {
 
 <script type="text/javascript">
 window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==70||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==70||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+/**
+ * For mac we need to ceck metakey
+ */
+window.addEventListener("keydown",function(e){if( event.metaKey&&(e.which==65||e.which==66||e.which==67||e.which==70||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==70||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+
 </script>
+
 
 <script type="text/javascript">
 document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
