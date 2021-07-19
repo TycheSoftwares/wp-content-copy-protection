e1c63a813888a4c2a904b495e2cfec0d0193d262
Shasvat <shasvat@tychesoftwares.com>
2021-07-19 17:48:40 +0530

    Updated compatibility for WP5.8

--- wpccpl.php
+++ wpccpl.php
@@ -10,7 +10,7 @@
  * Domain Path: /languages
  * Tags: content, protection,
  * Requires at least: 3.0.1
- * Tested up to:  5.6
+ * Tested up to:  5.8
  * Stable tag: 2.0.4
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html/
